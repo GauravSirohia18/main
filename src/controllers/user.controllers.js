d90ddee411b60ec480a124ac6285cb90a4961ef1
Gaurav Sirohia <gauravsirohia467@gmail.com>
2025-05-14 05:29:57 +0530

    CRUD added in User Schema

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -114,7 +114,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
 
     if (!email) {
-        throw new ApiError("E-mail not found");
+        throw new ApiError(400, "E-mail is required");
     }
     
     const user = await User.findOne({
@@ -228,7 +228,92 @@ const logOutUser = asyncHandler(async(req, res)=>{
 
 })
 
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+       const {oldPassword, newPassword} = req.body;
+       const user = await User.findById(req.user?._id)
+       const isPasswordValid = await user.isPasswordCorrect(oldPassword)
 
-export { registerUser , loginUser , refreshAccessToken , logOutUser};
+       if (!isPasswordValid) {
+           throw new ApiError(404, "Password is incorrect")
+       }
+
+       user.password = newPassword;
+
+       await user.save({validateBeforeSave : false})
+
+       return res.status(200).json(new ApiResponse(200, {}, "Password changed successfully"))    
+})
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res.status(200).json(new ApiResponse(200,req.user, "Current user details"))
+})
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+     const {fullname, email} = req. body;
+     if (!fullname || !email) {
+        throw new ApiError(400,"Fullname and email are required");
+     }
+     
+     const user = await User.findByIdAndUpdate(req.user._id, {
+        $set : { 
+            fullname,
+            email
+        }
+     },
+     {new : true}
+    ).select("-password -refreshToken")
+    
+    return res.status(200).json(new ApiResponse(200, user, "Account details updated successfully"))    
+})
+
+const updateUserAvatar = asyncHandler(async(req, res) => {
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "File is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(500, "Something went wrong while updating avatar");
+    }
+
+    const user = await User.findByIdAndUpdate(req.user?._id, {
+        $set : {
+            avatar : avatar.url
+        }
+    }, {new : true}).select("-password -refreshToken");
+
+    return res.status(200).json(new ApiResponse(200, user, "Avatar updated successfully"))    
+})
+
+const updateCoverImage = asyncHandler(async(req, res) => {
+    const coverImageLocalPath = req.files.coverImage[0].path;
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "File is required")
+    }
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!coverImage.url) {
+        throw new ApiError(500, "Something went wrong while updating coverimage");
+    }
+    
+    const user = await User.findByIdAndUpdate(req.user?._id, {
+        $set : {
+            coverImage : coverImage.url
+        }
+    }, {new : true}).select("-password -refreshToken");
+    
+    return res.status(200).json(new ApiResponse(200, user, "Cover image updated successfully"))    
+})
+
+
+
+
+export { registerUser , loginUser , refreshAccessToken , logOutUser, getCurrentUser, 
+    changeCurrentPassword, updateAccountDetails, updateUserAvatar, updateCoverImage
+};
 
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 
-import { registerUser, loginUser , refreshAccessToken, logOutUser} from "../controllers/user.controllers.js";
+import { registerUser, loginUser , refreshAccessToken, logOutUser, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateCoverImage} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
@@ -24,4 +24,22 @@ router.route("/logout").post(verifyJWT, logOutUser);
 
 router.route("/login").post(loginUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/get-current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-account-details").post(verifyJWT, updateAccountDetails);
+router.route("/update-avatar").post(
+  verifyJWT,
+  upload.fields([
+    { name: "avatar", maxCount: 1 }
+  ]),
+  updateUserAvatar
+);
+router.route("/update-coverImage").post(
+  verifyJWT,
+  upload.fields([
+    { name: "coverImage", maxCount: 1 }
+  ]),
+  updateCoverImage
+);
+
 export default router;
